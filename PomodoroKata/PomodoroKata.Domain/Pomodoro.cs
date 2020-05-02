918b9bbe1e8ace282b53b84f4fa035212317cb90
ambsenyestudi <adrifre7@hotmail.com>
2020-05-02 08:40:06 +0200

    Last25MinutesByDefault test green fase

--- PomodoroKata/PomodoroKata.Domain/Pomodoro.cs
+++ PomodoroKata/PomodoroKata.Domain/Pomodoro.cs
@@ -5,6 +5,9 @@ namespace PomodoroKata.Domain
     public class Pomodoro
     {
         public int DurationInMinutes { get; set; }
-        
+        public Pomodoro(int minutes=25)
+        {
+            DurationInMinutes = minutes;
+        }
     }
 }
